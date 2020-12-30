229c0627e9ea6a550445f3d4bc48e974038777eb
Safnas <iamsafnas@gmail.com>
2020-12-30 19:40:19 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { IWeatherData } from './models/IWeatherData.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'My Weather App';
+  cityDetails: IWeatherData;
+  ngOnInit() {
+  }
+
+  constructor(
+    private weatherService: WeatherService,
+  ) {}
+  
+  getCityDetails(woeid) {
+    this.weatherService.getCityDetails(woeid).subscribe(data =>{
+      this.cityDetails = data
+    })
+    /*
+      CHALLENGE
+       - pass the city id to service.getCityDetails(woeid)
+    */
+  }
+}
